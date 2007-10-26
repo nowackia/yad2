492f9f61923fb2163357ba53a1344142bb441c92
Adam Nowacki <nowackia.pw@gmail.com>
2007-10-26 18:06:38 +0000

    Alfa version of PictureButton control.

--- src/official/YAD2/Client/UI/MainForm.cs
+++ src/official/YAD2/Client/UI/MainForm.cs
@@ -10,9 +10,24 @@ namespace Client.UI
 {
     public partial class MainForm : Form
     {
+        PictureButton button = null;
+
         public MainForm()
         {
             InitializeComponent();
+
+            button = new PictureButton();
+            this.button.Location = new System.Drawing.Point(505, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 0;
+
+            this.button.Text = "Test text";
+
+            this.button.BackgroundImage = Image.FromFile("UI_Up.png");
+            this.button.PressedImage = Image.FromFile("UI_Up_Pressed.png");
+
+            this.Controls.Add(this.button);
         }
     }
 }
