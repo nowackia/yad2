c9e3ab6c3e965c527806956cebecd156f81f1015
pawel.rokoszny <pawel.rokoszny@e7e1d69a-f73b-0410-b7a5-4bea93d8f68f>
2007-10-27 17:58:01 +0000

    XMLLoader

---
+++ trunk/src/official/YAD2/Server/classes/Exceptions/XMLLoaderException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Server.classes.Exceptions
+{
+    class XMLLoaderException : XmlException
+    {
+        public XMLLoaderException(string message): base(message)
+        { }
+        public XMLLoaderException(Exception e)
+            : base(e.Message, e)
+        { }
+    }
+}
