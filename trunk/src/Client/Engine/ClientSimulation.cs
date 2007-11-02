dcdcee5fa9feab6b3f41907ecdb4fa487c3088ce
rrydziu <rrydziu@e7e1d69a-f73b-0410-b7a5-4bea93d8f68f>
2007-11-02 21:00:23 +0000

    aktualizacja wzgledem diagramów uml

---
+++ trunk/src/Client/Engine/ClientSimulation.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Engine
+{
+    class ClientSimulation:Simulation
+    {
+        /// <summary>
+        /// maintanance of animation, counting time to build etc.
+        /// </summary>
+        public override void Update()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
---
+++ trunk/src/Server/Engine/ServerSimulation.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Client.Engine;
+
+namespace Server.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class ServerSimulation:Simulation
+    {
+        public override void Update()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+    }
+}
