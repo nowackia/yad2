c44d8bca536a425585ca09ad798781c0e9920c10
slesinskik <slesinskik@e7e1d69a-f73b-0410-b7a5-4bea93d8f68f>
2007-11-05 00:12:40 +0000

    GameGraphics is completed. Waiting for some optimizations. It's time for renderers now!

---
+++ trunk/src/Client/Engine/Graphics/ClipRectangle.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Engine.GameGraphics {
+    public class ClipRectangle {
+        private float x;
+        private float y;
+        private float width;
+        private float height;
+
+        public ClipRectangle() {
+            x = y = 0.0f;
+            width = height = 1.0f;
+        }
+
+        public float X {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public float Y {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public float Width {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public float Height {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public float Left {
+            get { return X; }
+            set { X = value; }
+        }
+
+        public float Right {
+            get { return X + Width; }
+            set { X = value - Width; }
+        }
+
+        public float Top {
+            get { return Y + Height; }
+            set { Y = value - Height; }
+        }
+
+        public float Bottom {
+            get { return Y; }
+            set { Y = value; }
+        }
+    }
+}
